46df8d3980f49aecc3f9f3c916f85d80898a3505
Jens Horstmann <horstmann.jens.mail@gmail.com>
2016-01-24 12:37:54 +0100

    Init project

---
+++ MangaViewer/Classes/Helper/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangaViewer.Classes.Helper
+{
+    public static class Extensions
+    {
+    }
+}
---
+++ MangaViewer/Classes/View/MangaViewerWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace MangaViewer.Classes.View
+{
+    /// <summary>
+    /// Interaction logic for MangaViewerWindow.xaml
+    /// </summary>
+    public partial class MangaViewerWindow : Window
+    {
+        public MangaViewerWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
